34ab32d089907247a46597a19281ab1aee0dcb9b
weldoop <nayweldo@gmail.com>
2023-01-24 22:30:13 +0000

    desktop

--- resources/js/views/comingsoon.vue
+++ resources/js/views/comingsoon.vue
@@ -1,6 +1,7 @@
 <template>
 
 <!-- background div -->
+<div class="md:hidden ">
 <div class="flex flex-col items-center justify-center w-full h-full bg-black/60">
   <!-- background image -->
 
@@ -33,7 +34,7 @@
               <img src="../../../public/images/socials/instagram.png" alt="instagram" class="w-8 ">
             </a>
             <a href="https://www.twitter.com/">
-              <img src="../../../public/images/socials/twitter.png" alt="twitter" class="w-8">
+              <img src="https://twitter.com/IceCreamCrypto_" alt="twitter" class="w-8">
             </a>
             <!-- tiktok -->
             <a href="https://www.tiktok.com/">
@@ -44,12 +45,37 @@
             
         </div>
 </div>
+</div>
 
   
 </div>
 
+<!-- desktop -->
+
+<!-- full screen video  -->
 
 
+
+<header class="relative flex items-center justify-center h-screen overflow-hidden">
+  <div class="relative z-30 w-[40%] text-2xl text-white mt-44 rounded-xl">
+    
+    <form v-if=!posted @submit.prevent="createUser(form)" class="flex flex-col items-center w-full mt-4 text-lg rounded-md ">
+            
+            <input v-model="form.email" type="email" placeholder="Enter your email" class="w-full p-3 mb-2 text-center bg-white rounded ">
+            <button type="submit" class="w-full px-4 py-2 mb-2 text-white rounded-lg bg-lightblue hover:bg-darkblue">Join</button>
+            <div v-if="error">
+                <span class="font-bold text-red bg-black/60"> {{ error.email[0] }}</span>    
+            </div>
+        </form> 
+
+
+
+
+  </div>
+  <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full scale-50 max-w-none">
+    <source src="../../../public/video/ICCComingSoon.mp4" type="video/mp4" />Your browser does not support the video tag.
+  </video>
+</header>
 
 
 
