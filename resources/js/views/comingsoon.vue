667e65affdc3b3d58cfd5e2794917149964628e5
weldoop <nayweldo@gmail.com>
2023-01-29 17:18:42 +0000

    New Video

--- resources/js/views/comingsoon.vue
+++ resources/js/views/comingsoon.vue
@@ -1,66 +1,20 @@
 <template>
 
-<!-- background div -->
+<!-- mobile view with video  -->
+
 <div class="md:hidden ">
 <div class="flex flex-col items-center justify-center w-full h-full bg-black/60">
-  <!-- background image -->
 
-  <img src="../../../public/video/image00001.png" alt="background" class="object-cover w-full h-full ">
-
-  <!-- background video -->
-  
-
-  <!-- pusg -->
-    <!-- logo -->
-    <div  class="absolute w-[90%] mt-20  ">
-        <form v-if=!posted @submit.prevent="createUser(form)" class="flex flex-col items-center w-full mt-4 text-lg rounded-md ">
-            
-            <input v-model="form.email" type="email" placeholder="Enter your email" class="w-full p-3 mb-2 text-center bg-white rounded ">
-            <button type="submit" class="w-full px-4 py-2 mb-2 text-white rounded-lg bg-lightblue hover:bg-darkblue">Join</button>
-            <div v-if="error">
-                <span class="font-bold text-red bg-black/60"> {{ error.email[0] }}</span>    
-            </div>
-        </form> 
-
-        <div v-else class="flex flex-col items-center w-full mb-4 text-lg rounded-m">
-        <span class="font-bold text-white ">Thank you for joining us</span>
-        <span class="font-bold text-white ">We will be in touch soon!</span>
-        </div>
-
-       
-
-          <!-- social icons  -->
-          <div class="flex items-center justify-center w-full space-x-4 ">
-            <!-- telegram -->
-            <a href="https://www.telegram.com/">
-              <img src="../../../public/images/socials/telegram.png" alt="telegram" class="w-8 ">
-            </a>
-            <a href="https://instagram.com/icecreamcrypto_?igshid=YmMyMTA2M2Y=">
-              <img src="../../../public/images/socials/instagram.png" alt="instagram" class="w-8 ">
-            </a>
-            <a href="https://twitter.com/IceCreamCrypto_/">
-              <img src="../../../public/images/socials/twitter.png" alt="twitter" class="w-8">
-            </a>
-            <!-- tiktok -->
-            <a href="https://www.tiktok.com/">
-              <img src="../../../public/images/socials/tiktok.png" alt="tiktok" class="w-8 ">
-            </a>
-            
-            
-            
-        </div>
-</div>
-</div>
-
+ <video autoplay loop muted class="z-10">
+    <source src="../../../public/video/ICCComingSoonMobile.mp4" type="video/mp4" />Your browser does not support the video tag.
+  </video>
   
 </div>
-
-<!-- desktop -->
-
+</div>
 
 
 
-<header class="relative flex items-center justify-center h-screen overflow-hidden bg-black ">
+<header class="relative flex items-center justify-center invisible h-screen overflow-hidden bg-black md:visible ">
   <div class="relative z-30 w-[40%] text-2xl  mt-56 rounded-xl">
     
     <form v-if=!posted @submit.prevent="createUser(form)" class="flex flex-col items-center w-full text-lg rounded-md mt-28 ">
@@ -102,7 +56,7 @@
 
   </div>
   <video autoplay loop muted class="absolute z-10 invisible w-auto min-w-full min-h-full scale-50 md:visible max-w-none">
-    <source src="../../../public/video/ICCComingSoon.mp4" type="video/mp4" />Your browser does not support the video tag.
+    <source src="../../../public/video/ICCComingSoonDesktop.mp4" type="video/mp4" />Your browser does not support the video tag.
   </video>
   
 </header>
